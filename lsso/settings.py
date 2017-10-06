808f1c98e4f6d485d560a1d3994973d79dbb3fc8
Kaz Sto <kazuhisa@lazyarea.com>
2017-10-06 18:56:01 +0900

    add libs

--- lsso/settings.py
+++ lsso/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import pymysql
+import sys
 pymysql.install_as_MySQLdb()
+sys.path.append(os.path.join(os.path.dirname(__file__), 'libs'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
