1756610508953def19c6acc8de34da6ee47e2414
Kaz Sto <kazuhisa@lazyarea.com>
2017-10-06 19:47:45 +0900

    wip

---
+++ ldap/lib/ldapcli.py
@@ -0,0 +1,12 @@
+import datetime
+import os.path
+import re
+import logging
+import pymysql
+from django.conf import settings
+from ldap3 import Server, Connection, ALL, NTLM
+
+class ldapcli:
+    def sample():
+        print("---------")
+        pass
--- ldap/views.py
+++ ldap/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.conf import settings
-from ldap3 import Server, Connection, ALL, NTLM
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+from ldap.lib import ldapcli
 
 # Create your views here.
 def index(request):
+    ldapcli.sample()
     # return HttpResponse("Hello, world. You're at the news index.")
     latest_question_list = []
     template = loader.get_template('ldap/index.html')
