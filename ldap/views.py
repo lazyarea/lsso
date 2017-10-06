6f8bf037e949c840302c17a0d2f32415c3924f05
Kaz Sto <kazuhisa@lazyarea.com>
2017-10-06 17:59:42 +0900

    test

--- ldap/views.py
+++ ldap/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
+from ldap3 import Server, Connection, ALL, NTLM
 
 # Create your views here.
 def index(request):
@@ -13,4 +15,6 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def added(request):
+
+    print(settings.LDAP_AUTH_SEARCH_BASE)
     return HttpResponse("added.")
